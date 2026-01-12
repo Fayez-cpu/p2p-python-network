7dda1b3503b4f9c68b462783d2a8beb0d8f8a52e
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-01-12 09:34:59 +0000

    final

--- alarm.py
+++ alarm.py
@@ -3,7 +3,8 @@ import json
 import hmac_signature
 import datetime
 import key_hashing
-
+import os
+import requests
 hmac_key = b"2'\xae\x14\xbe\xb0\x0e\x88\xb4\xf6m\x10iL2\x0f\x85C\xfe\xa9\x16\xd5(H\x8aA*\xcf\r$\x1e\xf1"
 
 
@@ -11,28 +12,50 @@ class Alarm(Device):
     def __init__(self, discovery_port, tcp_port, buffer_size, device_id):
         self.device_id = device_id
         self.device_type = "alarm"
-        self.peers = []
         self.secret_key = "A5cf1hglf3!xgf"
         self.state = "idle"
-        self.nonces = []
-        super().__init__(discovery_port,tcp_port, buffer_size)
-
+        super().__init__(discovery_port,tcp_port, buffer_size, )
+    
+    
+    def log_event(self, event):
+        with open("logs/events.jsonl", "a") as f:
+            f.write(json.dumps(event) + "\n")
         
+
+    def send_log(self, log):
+        requests.post(
+            "http://localhost:3000/api/logs",
+            json=log,
+            timeout=2
+        )
+
     def check_entry(self, message):
         if isinstance(message, str):
             message = json.loads(message)
         print(message)
-        
+        timestamp = int(datetime.datetime.now().timestamp())
 
         if not key_hashing.check_rfid(message["rfid_tag"]):
             self.state = "sound alarm"
             print(self.state)
-            return "invalid tag"
+            event = {"timestamp": timestamp, "device_id": message["device_id"], "result": "Rejected", "reason": "RFID_WRONG" }
+            try:
+                self.send_log(event)
+            except:
+                print("error sending to api")
+            self.log_event(event)
+            return {"authorization": "invalid tag", "timestamp": int(datetime.datetime.now().timestamp()), "nonce": os.urandom(6).hex()}
         
         elif key_hashing.check_rfid(message["rfid_tag"]):
             self.state = "verified entry"
             print(self.state)
-            return "valid tag"
+            event = {"timestamp": timestamp, "device_id": message["device_id"], "result": "Allowed", "reason": "RFID_OK"}
+            try:
+                self.send_log(event)
+            except:
+                print("Error sending to api")
+            self.log_event(event)
+            return  {"authorization": "valid tag", "timestamp": timestamp, "nonce": os.urandom(6).hex()}
         
     def add_nonce(self, nonce):
         if len(self.nonces) >= 5:
@@ -40,7 +63,7 @@ class Alarm(Device):
         self.nonces.append(nonce)
 
         
-alarm = Alarm(5001, 5000, 1024,"front_01")
+alarm = Alarm(5001, 5005, 1024,"alarm")
  #alarm.check_entry({'msg_type': 'door_open', 'device_id': 'front_door_#51572AB', 'door_state': 'open', 'access_type': 'unauthorized', 'rfid_tag': None})
  # this above line is to test alarm with test json data
 alarm.run_tcp_server()
--- crypto/hmac.py
+++ crypto/hmac.py
@@ -1,13 +1,35 @@
 
 import hmac
 import hashlib
+import datetime
+
+
 
 def sign_message(key, payload ):
+    payload = bytes.fromhex(payload)
     signature = hmac.new(key, payload, hashlib.sha256).hexdigest()
     return signature
 
-def verify_hash(key, payload_bytes, signature_hex):
-    calc_sign = hmac.new(key, payload_bytes, hashlib.sha256).hexdigest()
-    return hmac.compare_digest(signature_hex, calc_sign)
+def verify_hash(key, payload):
+    ciphertext = bytes.fromhex(payload["ciphertext"])
+    signature = payload["hmac"]
+    calc_sign = hmac.new(key, ciphertext, hashlib.sha256).hexdigest()
+    return hmac.compare_digest(signature, calc_sign)
 
 
+def check_timestamp(plaintext, nonce_list):
+        now = int(datetime.datetime.now().timestamp())
+        if abs(now - plaintext["timestamp"]) > 4:
+            print("timestamp error")
+            return False
+        if plaintext["nonce"] in nonce_list:
+            print("nonce error")
+            return False
+        else:
+            add_nonce(nonce_list, plaintext["nonce"])
+        return True
+
+def add_nonce(nonce_list, nonce):
+        if len(nonce_list) >= 5:
+            nonce_list.pop(0)
+        nonce_list.append(nonce)
